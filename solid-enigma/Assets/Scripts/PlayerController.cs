bf406fd4c766bcf6dd6cd14d5adf6d921f63a7b5
Tyler Rusch <ruschta@uwec.edu>
2017-01-31 11:52:41 -0600

    added a base scene
    commit message woot

---
+++ solid-enigma/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public int oomph = 10;
+	[SerializeField]
+	private int foo = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newVelocity = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical")) * oomph;
+
+		rb.velocity = newVelocity;
+
+		
+	}
+
+	void FixedUpdate(){
+		
+	}
+}
