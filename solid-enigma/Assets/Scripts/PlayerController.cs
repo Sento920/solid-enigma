75b6a03d29d4bb890aad4f516988382919fc1b84
Tyler Rusch <sentokun9200@hotmail.com>
2017-02-02 08:18:37 -0600

    Pushing Player controller changes.

--- solid-enigma/Assets/Scripts/PlayerController.cs
+++ solid-enigma/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
-	public int oomph = 10;
 	[SerializeField]
-	private int foo = 10;
-
+	private int p_mov_const = 10;
+        private Vector3 p_heading;
 
 	// Use this for initialization
 	void Start () {
+                p_heading = new Vector3();
 		rb = GetComponent<Rigidbody> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newVelocity = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical")) * oomph;
-
-		rb.AddForce(newVelocity);
+                float logx = Input.GetAxis("Horizontal");
+                float logz = Input.GetAxis("Vertical");
+                //Input Checking
+                if(logx < 0.0f){
+                    p_heading = new Vector3( -1 * Math.Log10((logx * -1) +1), 0.0f, Math.Log10(logz))
+                }else if(logy < 0.0f){
+                    p_heading = new Vector3( Math.Log10(logx +1), 0.0f, -1 * Math.Log10((logz * -1) +1));
+                }else{
+                    p_heading = new Vector3( Math.Log10(logx + 1), 0.0f, Math.Log10(logz +1));
+                }
+		
 	}
 
 	void FixedUpdate(){
-		
+		//rb.AddForce(p_heading * p_mov_const);
+                rb.velocity = p_heading * p_mov_const;
 	}
 }
