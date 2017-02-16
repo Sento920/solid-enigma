6552538c5bfe7021c19c43dbcd182799b4240626
Tyler Rusch <ruschta@uwec.edu>
2017-02-16 12:00:47 -0600

    Changed Water, began changing movement

--- solid-enigma/Assets/Scripts/PlayerController.cs
+++ solid-enigma/Assets/Scripts/PlayerController.cs
@@ -7,36 +7,46 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 	[SerializeField]
-	private int p_mov_const = 10;
-        private Vector3 p_heading;
+	private int movConst = 10;
+    private Vector3 heading;
+
+	private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
-                p_heading = new Vector3();
+        heading = new Vector3();
 		rb = GetComponent<Rigidbody> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float logx = Input.GetAxis("Horizontal");
-        float logz = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
 
-		//Input Checking
-		if (logx < 0.0f && logz < 0.0f) {
-			p_heading = new Vector3 ( -1 * Mathf.Log10 ((logx * -1) + 1), 0.0f, -1 * Mathf.Log10 ((logz * -1) + 1));
-		}
-		else if(logx < 0.0f){
-			p_heading = new Vector3 ( -1 * Mathf.Log10 ((logx * -1) + 1), 0.0f, Mathf.Log10 (logz + 1));
-		}else if(logz < 0.0f){
-			p_heading = new Vector3( Mathf.Log10(logx + 1), 0.0f, -1 * Mathf.Log10((logz * -1) + 1));
-		}else{
-			p_heading = new Vector3( Mathf.Log10(logx + 1), 0.0f, Mathf.Log10(logz + 1));
-		}
-		
+		heading = new Vector3 (xAxis, 0.0f, zAxis);
+
+		transform.LookAt (this.transform.position + heading);
 	}
 
 	void FixedUpdate(){
-		rb.AddForce(p_heading * p_mov_const);
-                //rb.velocity = p_heading * p_mov_const;
+		//rb.AddForce(heading * movConst);
+		float logx = (heading.x * movConst) + velocity.x;
+		float logz = (heading.z * movConst) + velocity.z;
+
+
+
+		//Input Checking
+		if (logx < 0.0f && logz < 0.0f) {
+			velocity = new Vector3 ( -1 * Mathf.Log10 ((logx * -1) + 1), 0.0f, -1 * Mathf.Log10 ((logz * -1) + 1));
+		}
+		else if(logx < 0.0f){
+			velocity = new Vector3 ( -1 * Mathf.Log10 ((logx * -1) + 1), 0.0f, Mathf.Log10 (logz + 1));
+		}else if(logz < 0.0f){
+			velocity = new Vector3( Mathf.Log10(logx + 1), 0.0f, -1 * Mathf.Log10((logz * -1) + 1));
+		}else{
+			velocity = new Vector3( Mathf.Log10(logx + 1), 0.0f, Mathf.Log10(logz + 1));
+		}
+			
+        rb.velocity = velocity;
 	}
 }
--- solid-enigma/Assets/WaterController.cs
+++ solid-enigma/Assets/WaterController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class WaterController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
-
+		
     }
 
     void OnTriggerStay(Collider other) {
-        if(other.tag == "player" && other.attachedRigidbody) {
-            other.attachedRigidbody.AddForce(Vector3.up * 110);
+        if(other.attachedRigidbody) {
+			float forceApplied = Mathf.Max(-((other.transform.position.y - this.gameObject.transform.position.y) / 5.0f), 0.0f);
+			float bouyancy = other.attachedRigidbody.mass;
+			other.attachedRigidbody.AddForce(Vector3.up * (50000 * forceApplied * forceApplied));
+
         }
     }
 
