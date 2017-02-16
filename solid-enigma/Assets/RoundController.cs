fedb9e957d6aa235ac5f0cfa2caccad8dad06ad6
harrisna <harrisna@uwec.edu>
2017-02-15 23:44:47 -0600

    Added states to the roundcontroller
    Added states to the round controller so we can start to program each
    state the game can be in.

--- solid-enigma/Assets/RoundController.cs
+++ solid-enigma/Assets/RoundController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class RoundController : MonoBehaviour {
 
+    enum GameState { Playing, Shop, Paused };   // what other states do we need?  loading?  starting?  game over?
+
     private float timeLeft;
-    private bool timerActive = false;
+    private GameState state = GameState.Paused;
 
     [SerializeField] private float timeInMinutes = 5;
     [SerializeField] private GameObject sun;
@@ -13,18 +15,35 @@ public class RoundController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ResetTimer();
+        StartTimer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timeLeft -= Time.deltaTime;
+        if (state == GameState.Playing) {
+            timeLeft -= Time.deltaTime;
 
-        float sunAngle = (timeLeft / (timeInMinutes * 60));
-        sunAngle *= 180;
+            float sunAngle = (timeLeft / (timeInMinutes * 60));
+            sunAngle *= 180;
 
-        sun.transform.rotation = Quaternion.Euler(sunAngle, -30.0f, 0.0f);
+            sun.transform.rotation = Quaternion.Euler(sunAngle, -30.0f, 0.0f);
 
-        Debug.Log(getTime());
+            Debug.Log(getTime());
+
+            if (timeLeft < 0.0f)
+            {
+                state = GameState.Shop;
+
+                PauseTimer();
+                ResetTimer();
+
+                // TODO: setup shop (hah), and get ready for the next round
+            }
+        } else if (state == GameState.Shop) {
+            // TODO: shop related things, if they apply...
+        } else if (state == GameState.Paused) {
+            // look for unpause...
+        }
 	}
 
     void ResetTimer () {
@@ -33,17 +52,21 @@ public class RoundController : MonoBehaviour {
 
     void StartTimer () {
         // starts or resumes the timer
-        timerActive = true;
+        state = GameState.Playing;
     }
 
     void PauseTimer () {
         // pauses the timer
-        timerActive = false;
+        state = GameState.Paused;
     }
 
     string getTime () {
+        // i'm not sure about having a digital clock, it seems too precise.  maybe we should switch to an analog clock?
         int minutes = (int)Mathf.Floor(timeLeft / 60);
         int seconds = (int)timeLeft - (60 * minutes);
-        return minutes + ":" + seconds;
+        if (seconds >= 10)
+            return minutes + ":" + seconds;
+        else
+            return minutes + ":0" + seconds;
     }
 }
