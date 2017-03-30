6eb2520620ec9cf59bb09fccea5ca8ff8a6dba92
Tyler Rusch <ruschta@uwec.edu>
2017-03-29 20:47:28 -0500

    Added Preliminary UI
    started into 3d object UI. Wanted to consult on structure, and work on
    during class.

---
+++ solid-enigma/Assets/Scripts/GaugeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaugeScript : MonoBehaviour {
+
+    //Doing this following Chris's UI stuff from 3D. 
+    //http://www.twodee.org/blog/?p=13668
+
+    [SerializeField]
+    private float target;
+    private float angle;
+    [SerializeField]
+    private float maxVal;
+    [SerializeField]
+    private float minVal;
+    [SerializeField]
+    private float minAngle;
+    [SerializeField]
+    private float maxAngle;
+    [SerializeField]
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        angle = maxAngle;
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
