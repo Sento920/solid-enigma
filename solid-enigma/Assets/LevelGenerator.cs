47b0d64d3a6d2a17d14681e59ae95ee1c2161b5d
harrisna <harrisna@uwec.edu>
2017-03-29 22:53:32 -0500

    Prototype level generator
    Created a prototype level to demonstrate.  it's a little wonky

---
+++ solid-enigma/Assets/LevelGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    [SerializeField] private GameObject tile;
+    [SerializeField] private int width = 200;
+    [SerializeField] private int height = 100;
+
+    private int[] map;
+
+	// Use this for initialization
+	void Start () {
+        GenerateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RecursiveGenerateStreets(int startx, int endx, int starty, int endy, bool vertical) {
+        if (((endx - startx <= 2 && endy - starty <= 8) || (endy - starty <= 2 && endx - startx <= 8)))
+            return;
+
+        int splitRoad;
+
+        if (vertical)
+            splitRoad = Random.Range(startx, endx - 1);
+        else
+            splitRoad = Random.Range(starty, endy - 1);
+
+        // fill road
+        if (vertical) {
+            for (int i = starty; i < endy; i++) {
+                map[(width * i) + splitRoad] = 0;
+            }
+
+            RecursiveGenerateStreets(startx, splitRoad, starty, endy, !vertical);
+            RecursiveGenerateStreets(splitRoad, endx, starty, endy, !vertical);
+        } else {
+            //Debug.Log(splitRoad + " " + startx + " " + endx + " " + endy + " " + starty);
+            for (int i = startx; i < endx; i++) {
+                map[(width * splitRoad) + i] = 0;
+            }
+
+            RecursiveGenerateStreets(startx, endx, starty, splitRoad, !vertical);
+            RecursiveGenerateStreets(startx, endx, splitRoad, endy, !vertical);
+        }
+    }
+
+    void GenerateLevel()
+    {
+        map = new int[width * height];
+
+        for (int i = 0; i < width * height; i++)
+            map[i] = 1;
+
+        RecursiveGenerateStreets(0, width, 0, height, true);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (map[(width * y) + x] == 1)
+                    Instantiate(tile, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
+    }
+}
