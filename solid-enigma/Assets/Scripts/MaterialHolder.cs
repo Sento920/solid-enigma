32cb5db51a85407ebef97f9a6f35d85e8df67ef2
Tyler Rusch <ruschta@uwec.edu>
2017-05-01 23:18:42 -0500

    Added Person color randomization. More colors needed.

---
+++ solid-enigma/Assets/Scripts/MaterialHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialHolder : MonoBehaviour {
+
+    public Material[] Array1;
+    public Material[] Array2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Material[] getA1() {
+        return Array1;
+    }
+
+    public Material[] getA2(){
+        return Array2;
+    }
+}
---
+++ solid-enigma/Assets/Scripts/PersonRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonRandomizer : MonoBehaviour {
+    GameObject MaterialHolder;
+    private Material head;
+    private Material body;
+    
+	// Use this for initialization
+	void Start () {
+        MaterialHolder = GameObject.FindWithTag("Material");
+        Material[] bodyColors = MaterialHolder.GetComponent<MaterialHolder>().getA1();
+        Material[] headColors = MaterialHolder.GetComponent<MaterialHolder>().getA2();
+        GetComponent<Renderer>().material = bodyColors[Random.Range(0,bodyColors.Length)];
+        GetComponentInChildren<Renderer>().material = headColors[Random.Range(0, headColors.Length)];
+	}
+
+}
